ff3e86e002f4b6c21afca341f523800830294fa8
ambrebaumann <95317739+ambrebaumann@users.noreply.github.com>
2021-12-01 09:43:05 +0100

    Add files via upload

---
+++ module_transcription.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "module_score.c"
+
+int seq_codante(char* argv[]) {
+/*A optimiser quand fichier fait*/
+/*Fonction qui retourne 1 si la séquence ADN est codante (longueur divisible par 3 et a un codon d'initiation*/
+	int ls = longueur_seq(argv[1]); /*initie la longueur de la chaîne*/
+	char codon1[3];
+	lire_seq(argv, 3, codon1);
+	
+	int booleen=0;
+	if (ls%3==0 && codon1=='ATG'){ 
+		booleen=1;
+	}
+	return booleen;
+}
+
+
+void module_transcrit(char** argv[]) {
+
+/*Redemander à l'utilisateur une séquence si elle est non codante*/
+	int ls = longueur_seq(argv); /*i!!!!!!nitie la longueur de la chaîne*/
+	char atranscrire[ls];
+	lire_seq(argv, ls, atranscrire);
+	
+	printf(atranscrire);
+	/*Mettre ici suppression d'un fichier transcrit existant*/
+	
+	FILE * fp = fopen ("transcrit.txt", "a"); /*Regarder pour trouver u moyer de changer le nom du fichier ou cleaner env travail*/
+	if (!fp) {
+		printf ("L’ouverture a échoué.\n") ;
+		exit ( EXIT_FAILURE );	
+	}
+	
+	for (int i=0; i<ls; i++){
+	 	if (atranscrire[i]=="T")
+	 		fprintf(fp, "U");
+	 	else 
+	 		fprintf(fp, atranscrire[i]);
+	 	if (i%80==0)
+	 		fprintf(fp, "\n");
+	}
+	fclose(fp);
+}
+
+
+
+int main(int argc, char * argv[]) {
+	//module_score_id(argv);
+	module_transcrit(argv);
+}	
+
+
+
+
+
+
