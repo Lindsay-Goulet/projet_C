0bcb1e0b948eaeed44bdf6c3a63e50c4a6edd09e
ambrebaumann <ambrebmn99@gmail.com>
2021-12-01 22:00:33 +0100

    aaaaa

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int seq_codante() {
+    char seq[9];
+    seq=
+	int booleen=0; /*Initie un booléen*/
+	if (ls%3==0 && strcmp(codon1, "ATG") == 0){ /*Vérifie si séquence codante et si oui bool=1*/
+		booleen=1;
+	}
+	return booleen; /*Retourne le booléen*/
+}
