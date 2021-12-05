47369ebf74de051c7da28fd13d969bcd95f48f4a
Lindsay Goulet <lindsay.goulet@universite-paris-saclay.fr>
2021-12-05 12:23:00 +0100

    module score id

---
+++ module_score_identite.h
@@ -0,0 +1,11 @@
+#ifndef module_score_identite
+#define module_score_identite
+
+double pourct_id(double ls, int nb_id);
+
+int nb_nucleotide_ident(char* sequence1, char* sequence2, taille_seq);
+
+int longueur_sequence(const char* path_input);
+
+
+#endif
