cdc0ee65e2b1c3b36a2acdcda41ef2ed41ad518d
Lindsay Goulet <lindsay.goulet@universite-paris-saclay.fr>
2021-12-05 22:55:17 +0100

    modif

--- module_score_identite.c
+++ module_score_identite.c
@@ -2,10 +2,7 @@
 #include <stdlib.h>
 #include "utiles.h"
 
-double pourct_id(double taille_seq, int nb_id) {
-	double id = nb_id*100/taille_seq; /*produit en croix pour calculer le pourcentage*/
-	return id;
-}
+
 
 int nb_nucleotide_ident(char* sequence1, char* sequence2, int taille_seq) {
 	int i;
@@ -18,25 +15,7 @@ int nb_nucleotide_ident(char* sequence1, char* sequence2, int taille_seq) {
 	return identique;
 }
 
-int longueur_sequence(const char* path_input) {
-    /*ajouter procédure qui supprime la ligne avant la sequence*/
-    int taille_seq=0;
-	int c;
 
-    FILE* f = fopen(path_input, "r"); /* ouverture du fichier*/
-	if (!f) {
-		printf("L'ouverture a echoué.\n");
-		exit(EXIT_FAILURE);
-	}
-
-	while ((c=fgetc(f)) != EOF) { /*je parcours tout le fichier et je compte les éléments*/
-		if (c != '\n') {
-			taille_seq++;
-		}
-	}
-	
-	return taille_seq; 
-}
 
 void seq_id(int taille_seq, char* seq1, char* seq2, char* id) {
 	int i;
@@ -76,9 +55,3 @@ void module_score_id(const char* path_input, const char* path_input2) {
 	for (i=0; i<taille_seq; i++) { printf("%c", id[i]); } printf("\n");
 }
 
-int main('f1.txt') {
-	printf("Avec quelle séquence voulez-vous comparer ?");
-	char path_input2[30];
-	get_path_from_user(char* path_input2);
-	module_score_id(path_input, path_input2);
-}
