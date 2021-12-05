e6059fee4ce5f75e1c21120ffc30ee47f1bfb08d
Lindsay Goulet <lindsay.goulet@universite-paris-saclay.fr>
2021-12-05 21:26:17 +0100

    ajout fcts

--- module_score_identite.h
+++ module_score_identite.h
@@ -1,11 +1,15 @@
 #ifndef module_score_identite
 #define module_score_identite
 
-double pourct_id(double ls, int nb_id);
+double pourct_id(double taille_seq, int nb_id);
 
-int nb_nucleotide_ident(char* sequence1, char* sequence2, taille_seq);
+int nb_nucleotide_ident(char* sequence1, char* sequence2, int taille_seq);
 
 int longueur_sequence(const char* path_input);
+
+void seq_id(int taille_seq, char* seq1, char* seq2, char* id);
+
+void module_score_id(const char* path_input, const char* path_input2);
 
 
 #endif
