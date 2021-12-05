4ea0db24a88c877ff98f447dafa59f90504520b3
Lindsay Goulet <lindsay.goulet@universite-paris-saclay.fr>
2021-12-05 22:55:13 +0100

    modif

--- main.c
+++ main.c
@@ -1,53 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "utiles.h"
+#include "module_transcription.h"
+#include "module_score_identite.h"
+#include "module_score_polarite.h"
 
-/* il fait #include tous nos fichiers .h*/
+/* il faut #include tous nos fichiers .h*/
 
 
 int main() {
 	int module_number=0;
     char path_input[30];
-    printf("Bonjour et bienvenue dans le logiciel d'analyse de séquences.");
-    get_path_from_user(path_input); /*on demande à l'utilisateur la séquence à étudier*/
-	get_module_number_from_user(module_number); /*on demande à l'utilisateur le module qu'il veut utiliser*/
-	
-	    switch(numero_module)
-        {
-            case 1:
-                /*instruction*/
-                break;
+    char path_output[30];
+    printf("Bonjour et bienvenue dans le logiciel d'analyse de séquences.\n");
+	get_module_number_from_user(&module_number); /*on demande à l'utilisateur le module qu'il veut utiliser*/
 
-            case 2:
-           	    /*instruction*/;
-                break;
-
-            case 3:
-                /*instruction*/;
-                break;
-
-            case 4:
-                printf("Avec quelle séquence voulez-vous comparer ?");
-	            char path_input2[30];
-	            get_path_from_user(char* path_input2);
-	            module_score_id(path_input, path_input2);
-                break;
-            
-            case 5:
-                /*instruction*/;
-                break;
-            
-            case 6:
-                /*instruction*/;
-                break;
-            
-            case 7:
-                /*instruction*/;
-                break;
-
-            /* si aucun des 7 */
-            default:
-                printf("Erreur ! Aucun module ne correspond à votre demande.\nRéessayez.\n\n");
+	switch(module_number)
+    {
+        case 1: {
+            get_path_from_user(path_input); /*on demande à l'utilisateur le fichier de la séquence à étudier*/
+            printf("Choisissez le nom de votre fichier pour la séquence transcrite.\n");
+            get_path_from_user(path_output);
+            module_transcription(path_input, path_output);
+            break;
         }
+        case 2: {
+           	/*instruction*/;
+            break;
+        }
+        case 3: {
+            /*instruction*/;
+            break;
+        }
+        case 4: {
+            get_path_from_user(path_input); /*on demande à l'utilisateur le fichier de la séquence à étudier*/
+            printf("Avec quelle séquence voulez-vous comparer ?\n");
+	        char path_input2[30];
+	        get_path_from_user(path_input2);
+	        module_score_id(path_input, path_input2);
+            break;
+        }
+        case 5: {
+            get_path_from_user(path_input); /*on demande à l'utilisateur le fichier de la séquence à étudier*/
+            printf("Avec quelle séquence voulez-vous comparer ?\n");
+	        char path_input2[30];
+	        get_path_from_user(path_input2);
+            module_score_pol(path_input, path_input2);
+            break;
+        }
+        case 6: {
+            /*instruction*/;
+            break;
+        }
+        case 7: {
+            /*instruction*/;
+            break;
+        }
+            /* si aucun des 7 */
+        default:
+            printf("Erreur ! Aucun module ne correspond à votre demande.\nRéessayez.\n\n");
     }
 }
