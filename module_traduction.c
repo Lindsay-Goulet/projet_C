02e13540b6eda93955edd33a39f6e65ab173af98
ambrebaumann <ambrebmn99@gmail.com>
2021-12-06 16:44:50 +0100

    bjr

--- module_traduction.c
+++ module_traduction.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <search.h> /*Permet la création de dictionnaire avec différentes fonctions*/
 #include "utiles.h"
 
 #define nb_aa 64
 
+/*
+void dico_aa(int taille_seq){
 
-void dico_aa(){
 
-/*BONJOUR*/
 
 
 }
-
+*/
 
 int main() {
 
-    
+    char path_input[30];
+
+    int taille_seq = longueur_sequence(path_input);
+    char seq[taille_seq];
+    extract_sequence(path_input, seq1);
+
+    for (i=0, i<taille_seq-2, i=i+3) {
+        
 
 
+
+    }
 
 }
