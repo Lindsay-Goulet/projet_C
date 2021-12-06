a10b5037102bb3201384e63fc94565137ec52c17
ambrebaumann <ambrebmn99@gmail.com>
2021-12-06 15:42:01 +0100

    bjr

---
+++ module_traduction.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <search.h> /*Permet la création de dictionnaire avec différentes fonctions*/
+#include "utiles.h"
+
+#define nb_aa 64
+
+dict_aa () {
+    hcreate(nb_codon);
+    char codon[] = {"UUU", "UUC", "UUA", "UUG", "UCU", "UCC", "UCA", "UCG", "UAU", "UAC", "UAA", "UAG", "UGU", "UGC", "UGA", "UGG",
+                    "CUU", "CUC", "CUA", "CUG", "CCU", "CCC", "CCA", "CCG", "CAU", "CAC", "CAA", "CAG", "CGU", "CGC", "CGA", "CGG",
+                    "AUU", "AUC", "AUA", "AUG", "ACU", "ACC", 
+
+
+    }
+
+
+
+}
+
+
+
+
+int main() {
+
+
+}
