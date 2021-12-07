db8d1bad1508c2bbc2fcda79940d4318f0ea13f9
Lindsay Goulet <lindsay.goulet@universite-paris-saclay.fr>
2021-12-07 17:54:42 +0100

    modif

--- module_recherche_sequence_codante_maximale.h
+++ module_recherche_sequence_codante_maximale.h
@@ -1,7 +1,19 @@
 #ifndef module_sequence_codante_maximale
 #define module_sequence_codante_maximale
 
+void codons_start_stop_brin_sens(char* sequence, int taille_seq, int* start, int* stop, int* longueur);
 
+void codons_start_stop_brin_antisens(char* sequence, int taille_seq, int* start, int* stop, int* longueur);
+
+int codons_start_stop_ORF_max(char* sequence, int taille_seq, int* start, int* stop, int* taille);
+
+void creation_sequence_ORF_sens(int taille_seq_codante, char* sequence, int start, int stop, char* seq_cod);
+
+void creation_sequence_ORF_antisens(int taille_seq_codante, char* sequence, int start, int stop, char* seq_cod)
+
+void module_recherche_sequence_codante_maximale();
+
+#endif
 
 
 #endif
