d7ab2bb5e2e423f57546309ab5bb9ed4e31143d4
Lindsay Goulet <lindsay.goulet@universite-paris-saclay.fr>
2021-12-07 18:15:02 +0100

    modif

--- main.c
+++ main.c
@@ -10,43 +10,50 @@
 /* il faut #include tous nos fichiers .h*/
 
 int main() {
-	int module_number=0;
-    printf("Bonjour et bienvenue dans le logiciel d'analyse de séquences.\n");
-	get_module_number_from_user(&module_number); /*on demande à l'utilisateur le module qu'il veut utiliser*/
+    char continue;
+    do {
+	    int module_number=0;
+        printf("Bonjour et bienvenue dans le logiciel d'analyse de séquences.\n");
+	    get_module_number_from_user(&module_number); /*on demande à l'utilisateur le module qu'il veut utiliser*/
 
-	switch(module_number)
-    {
-        case 1: {
-            module_recherche_sequence_codante_maximale();
-            break;
-        }
-        case 2: {
-            /*module_transcription();*/
-            break;
-        }
-        case 3: {
-            /*module_traduction;*/
-            break;
-        }
-        case 4: {
-	        module_score_id();
-            break;
-        }
-        case 5: {
-            module_score_pol();
-            break;
-        }
-        case 6: {
-            /*instruction*/;
-            break;
-        }
-        case 7: {
-            /*instruction*/;
-            break;
-        }
+	    switch(module_number)
+        {
+            case 1: {
+                module_recherche_sequence_codante_maximale();
+                break;
+            }
+            case 2: {
+                /*module_transcription();*/
+                break;
+            }
+            case 3: {
+                /*module_traduction;*/
+                break;
+            }
+            case 4: {
+	            module_score_id();
+                break;
+            }
+            case 5: {
+                module_score_pol();
+                break;
+            }
+            case 6: {
+                /*instruction*/;
+                break;
+            }
+            case 7: {
+                /*instruction*/;
+                break;
+            }
             /* si aucun des 7 */
-        default: {
-            printf("Erreur ! Aucun module ne correspond à votre demande.\nRéessayez.\n\n");
+            default: {
+                printf("Erreur ! Aucun module ne correspond à votre demande.\nRéessayez.\n\n");
+            }
         }
+        printf("\nVoulez-vous utiliser un autre module ? (o/n)\n");
+        scanf("%c", &continue);
     }
+    while (continue == 'o');
+    
 }
