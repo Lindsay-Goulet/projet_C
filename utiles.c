079a0019545d3f699dc69cdaddd74420f0dad97f
Lindsay Goulet <lindsay.goulet@universite-paris-saclay.fr>
2021-12-09 17:43:14 +0100

    modif

--- utiles.c
+++ utiles.c
@@ -25,7 +25,7 @@ void get_path_from_user(char* path_input) {
 }
 
 void get_module_number_from_user(char* module_number) {
-	    printf(CYN underscore "\nQuel module voulez-vous utiliser ?"RESET"\n1. Recherche de la séquence codante de taille maximale.\n2. Transcription d’une séquence ADN en séquence ARN.\n3. Traduction d’une séquence codante en séquence protéique.\n4. Calcul du score d’identité entre deux séquences.\n5. Calcul du score de similarité de polarité entre deux séquences protéiques.\n6. Recherche d’une séquence consensus à partir d’un alignement multiple.\n7. Recherche de la plus grande sous-chaîne de polarité commune à 2 séquences protéiques.\nq : Quitter."BLU blink bright"Module : "RESET);
+	    printf(CYN underscore "\nQuel module voulez-vous utiliser ?"RESET"\n1. Recherche de la séquence codante de taille maximale.\n2. Transcription d’une séquence ADN en séquence ARN.\n3. Traduction d’une séquence codante en séquence protéique.\n4. Calcul du score d’identité entre deux séquences.\n5. Calcul du score de similarité de polarité entre deux séquences protéiques.\n6. Recherche d’une séquence consensus à partir d’un alignement multiple.\n7. Recherche de la plus grande sous-chaîne de polarité commune à 2 séquences protéiques.\nq : Quitter.\n"BLU blink bright"Module : "RESET);
         scanf("\n%c", module_number);
 }
 
@@ -64,7 +64,7 @@ void save_sequence(const char* path_output, char* sequence) {
 	while (sequence[i] != '\0') { /*on parcourt toute la séquence stockée dans la variable séquence*/
         	fputc(sequence[i], f);
         	i++;
-        	if ((i%80 == 0) { /*quand on arrive au 80e caractère, on passe à la ligne*/
+        	if (i%80 == 0) { /*quand on arrive au 80e caractère, on passe à la ligne*/
             		fprintf(f, "\n");
         	}
     }
