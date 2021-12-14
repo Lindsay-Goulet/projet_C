970c0de69374cac54e367ee59d6b8edb678ecae9
ambrebaumann <ambrebmn99@gmail.com>
2021-12-14 18:25:07 +0100

    commentaires

--- module_recherche_sequence_codante_maximale.c
+++ module_recherche_sequence_codante_maximale.c
@@ -3,29 +3,34 @@
 #include "utiles.h"
 
 void codons_start_stop_brin_sens(char* sequence, int taille_seq, int* start, int* stop, int* longueur) {
+	//Procédure qui permet de trouver le couple de codons START et STOP de la séquence ADN dans le brin sens de la plus grande longueur
+
 	int i;
 	int j;
 	int st;
 	int sp;
 	int l;
 	int longueur_maximale=0;
-	for (i=0; i<taille_seq-2; i++) { /*on parcourt la séquence*/
-		if ((sequence[i] == 'A') && (sequence[i+1] == 'T') && (sequence[i+2] == 'G')) { 
-			st = i;
-			j = st+3;
+	
+	for (i=0; i<taille_seq-2; i++) { //On parcourt la séquence
+
+		if ((sequence[i] == 'A') && (sequence[i+1] == 'T') && (sequence[i+2] == 'G')) { //codon START
+			st = i; //Si START trouvé st prend la valeur i càd l'endroit où est le début du codon START
+			j = st+3; //Pour continuer après le codon START afin de trouve un STOP
+			
 			while ((j<taille_seq-2) && !(((sequence[j] == 'T' && sequence[j+1] == 'A' && sequence[j+2] == 'A') 
 			|| (sequence[j] == 'T' && sequence[j+1] == 'A' && sequence[j+2] == 'G') 
 			|| (sequence[j] == 'T' && sequence[j+1] == 'G' && sequence[j+2] == 'A'))))
-				j+=3;
+				j+=3; //Tant que pas de STOP on continue à parcourir la séquence 
 
 			if (((sequence[j] == 'T' && sequence[j+1] == 'A' && sequence[j+2] == 'A') 
 			|| (sequence[j] == 'T' && sequence[j+1] == 'A' && sequence[j+2] == 'G') 
 			|| (sequence[j] == 'T' && sequence[j+1] == 'G' && sequence[j+2] == 'A'))) {/*on a trouvé un codon stop*/
-				sp = j;
+				sp = j; //Si STOP trouvé sp prend la valeur j càd l'endroit où est le début du codon STOP
 
 				if ((l = abs(st-sp)+3) > longueur_maximale) {
-					longueur_maximale = l; /*on prend le couple start-stop ayant la plus grande longueur*/
-					*start = st; /*on modifie les valeurs de start et stop*/
+					longueur_maximale = l; //On prend le couple start-stop ayant la plus grande longueur
+					*start = st; //On modifie les valeurs de start, stop et longueur_maximale
 					*stop = sp;
 					*longueur = longueur_maximale;
 					
@@ -36,14 +41,19 @@ void codons_start_stop_brin_sens(char* sequence, int taille_seq, int* start, int
 }
 
 void codons_start_stop_brin_antisens(char* sequence, int taille_seq, int* start, int* stop, int* longueur) {
+	//Procédure qui permet de trouver le couple de codons START et STOP de la séquence ADN dans le brin antisens de la plus grande longueur 
+	//Pour les commentaires : voir les commentaires de la procédure codons_start_stop_brin_sens
+
 	int i;
 	int j;
 	int st;
 	int sp;
 	int l;
 	int longueur_maximale=0;
+	
 	for (i=taille_seq-1; i>1; i--) { /*on parcourt la séquence*/
-		if ((sequence[i] == 'T') && (sequence[i-1] == 'A') && (sequence[i-2] == 'C')) { 
+		
+		if ((sequence[i] == 'T') && (sequence[i-1] == 'A') && (sequence[i-2] == 'C')) { //ATG devient TAC sur le brin antisens
 			st = i;
 			j = st-3;
 			
@@ -54,12 +64,12 @@ void codons_start_stop_brin_antisens(char* sequence, int taille_seq, int* start,
 
 			if (((sequence[j] == 'A' && sequence[j-1] == 'T' && sequence[j-2] == 'T') 
 			|| (sequence[j] == 'A' && sequence[j-1] == 'T' && sequence[j-2] == 'C') 
-			|| (sequence[j] == 'A' && sequence[j-1] == 'C' && sequence[j-2] == 'T'))) {/*on a trouvé un codon stop*/
+			|| (sequence[j] == 'A' && sequence[j-1] == 'C' && sequence[j-2] == 'T'))) {
 				sp = j;
 
 				if ((l = abs(st-sp)+3) > longueur_maximale) {
-					longueur_maximale = l; /*on prend le couple start-stop ayant la plus grande longueur*/
-					*start = st; /*on modifie les valeurs de start et stop*/
+					longueur_maximale = l; 
+					*start = st; 
 					*stop = sp;
 					*longueur = longueur_maximale;
 					
@@ -70,6 +80,8 @@ void codons_start_stop_brin_antisens(char* sequence, int taille_seq, int* start,
 }
 
 void sequence_complementaire(char * sequence, int taille_seq) {
+	//Procédure qui permet de créer la séquence complémentaire 
+
 	int i;
 	for (i=0; i<taille_seq; i++) {
 		if (sequence[i] == 'A') sequence[i] = 'T';
@@ -79,8 +91,9 @@ void sequence_complementaire(char * sequence, int taille_seq) {
 	}
 }
 
-
 int codons_start_stop_ORF_max(char* sequence, int taille_seq, int* start, int* stop, int* taille) {
+	//Fonction qui permet de sélectionner la plus grande ORF parmis celles trouvés dans les brins sens et antisens 
+
 	int start_sens = -1;
 	int stop_sens = -1;
 	int start_antisens = -1;
@@ -88,22 +101,22 @@ int codons_start_stop_ORF_max(char* sequence, int taille_seq, int* start, int* s
 	int longueur_sens = -1;
 	int longueur_antisens = -1;
 	
-	codons_start_stop_brin_sens(sequence, taille_seq, &start_sens, &stop_sens, &longueur_sens);
+	codons_start_stop_brin_sens(sequence, taille_seq, &start_sens, &stop_sens, &longueur_sens); //On récupère les codons START, STOP et la longueur de l'ORF dans les brins sens et antisens 
 	codons_start_stop_brin_antisens(sequence, taille_seq, &start_antisens, &stop_antisens, &longueur_antisens);
 	
 	if (longueur_sens != -1 && longueur_antisens != -1) {
 		
-		if (longueur_sens >= longueur_antisens) {
+		if (longueur_sens >= longueur_antisens) { 
 			*start = start_sens;
 			*stop = stop_sens;
 			*taille = longueur_sens;	
-			return 2;
+			return 2; //2 si ORF trouvée dans le brin sens
 		}
 		else {
 			*start = start_antisens;
 			*stop = stop_antisens;
 			*taille = longueur_antisens;
-			return 1;
+			return 1; //1 si ORF trouvée dans le brin antisens
 		}
 	}
 	else {
@@ -111,23 +124,24 @@ int codons_start_stop_ORF_max(char* sequence, int taille_seq, int* start, int* s
 			*start = start_sens;
 			*stop = stop_sens;
 			*taille = longueur_sens;
-			return 2;
+			return 2; //2 si ORF trouvée dans le brin sens
 		}
 		else {
 			if (longueur_sens == -1 && longueur_antisens != -1)  {
 				*start = start_antisens;
 				*stop = stop_antisens;
 				*taille = longueur_antisens;
-				return 1;
+				return 1; //1 si ORF trouvée dans le brin antisens
 			}
 			else { 
-				return 0;
+				return 0; //0 si aucune ORF n'est trouvée et donc que la séquence n'est pas codante
 			}
 		}
 	}
 }
 
 void creation_sequence_ORF_sens(int taille_seq_codante, char* sequence, int start, int stop, char* seq_cod) {
+	//Procédure qui permet de créer un tableau de caractères contenant l'ORF dans le brin sens
 	int j=0;
 	int i;
 		for (i=start; i<stop+3; i++) {
@@ -138,18 +152,23 @@ void creation_sequence_ORF_sens(int taille_seq_codante, char* sequence, int star
 }
 
 void creation_sequence_ORF_antisens(int taille_seq_codante, char* sequence, int start, int stop, char* seq_cod) {
+	//Procédure qui permet de créer un tableau de caractères contenant l'ORF dans le brin antisens
 	int j=taille_seq_codante-1;
 	int i;
 	for (i=stop-2; i<start+1; i++) {
 		seq_cod[j] = sequence[i];
 		j--;
 	}
-	sequence_complementaire(seq_cod, taille_seq_codante);
+	sequence_complementaire(seq_cod, taille_seq_codante); //Prend la séquence complémentaire de l'ORF qui a été mis dans le bon sens (5' -> 3')
 	seq_cod[taille_seq_codante] = '\0';
 }
 
 
 void module_recherche_sequence_codante_maximale() {
+	/* - Procédure qui demande en entrée un fichier contenant une séquence ADN au format FASTA.
+	   - Recherche dans la séquence ADN la plus grande séquence codante
+	   - Et renvoie dans un fichier cette ORF la plus grande*/
+
 	int start;
 	int stop;
 	int taille_seq_codante;
@@ -157,14 +176,13 @@ void module_recherche_sequence_codante_maximale() {
     char path_input[30];
     char path_output[30];
 
-    get_path_from_user(path_input);
+    get_path_from_user(path_input); //Demande la séquence ADN à tester
 
-    int taille_seq = longueur_sequence(path_input);
+    int taille_seq = longueur_sequence(path_input); //Initialise la longueur de la séquence
     char sequence[taille_seq+1];
-	extract_sequence(path_input, sequence);
+	extract_sequence(path_input, sequence); //Permet de stocker la séquence ADN dans un tableau
 	
-	int codante = codons_start_stop_ORF_max(sequence, taille_seq, &start, &stop, &taille_seq_codante); 
-	/*renvoie 2 si codant sur brin sens, 1 si codant sur brin antisens, 0 si non codant*/
+	int codante = codons_start_stop_ORF_max(sequence, taille_seq, &start, &stop, &taille_seq_codante); //Renvoie 2 si codant sur brin sens, 1 si codant sur brin antisens, 0 si non codant
 	char seq_cod[taille_seq_codante+1];
 	printf("Start = %d, Stop = %d\n", start, stop);
 
--- module_score_identite.c
+++ module_score_identite.c
@@ -11,10 +11,11 @@
 #define bright      "\x1b[1m"
 
 int nb_nucleotide_ident(char* sequence1, char* sequence2, int taille_seq) {
+	//Fonction qui permet de compter entre deux séquences les nucléotides identiques.
 	int i;
 	int identique=0;
 	for (i=0; i<taille_seq; i++) { 
-		if (sequence1[i] == sequence2[i]) { /*je parcours mes séq et je compte le nb de différence entre les deux */
+		if (sequence1[i] == sequence2[i]) { //Je parcours mes séq et je compte le nb de différence entre les deux
 			identique++;
 		}
 	}
@@ -24,10 +25,11 @@ int nb_nucleotide_ident(char* sequence1, char* sequence2, int taille_seq) {
 
 
 void seq_id(int taille_seq, char* seq1, char* seq2, char* id) {
+	//Procédure qui permet d'écrire les nucléotides identiques dans le terminal, et si ils sont différents met un -
 	int i;
 	for (i=0; i<taille_seq; i++) {
-		if (seq1[i] == seq2[i]) { /*ça c'est pour écrire les séq dans le terminal. Je créé ma seq "id" donc je mets le nucléo quand il est identique et sinon je mets un - */
-			id[i] = seq1[i];
+		if (seq1[i] == seq2[i]) { 
+			id[i] = seq1[i]; 
 		}
 		else {
 			id[i] = '-';
@@ -36,31 +38,34 @@ void seq_id(int taille_seq, char* seq1, char* seq2, char* id) {
 }
 
 void module_score_id() {
-
+	/* - Procédure qui demande à l'utilisateur deux fichiers contenant chacune une séquence.
+	   - Les séquences doivent être de même taille. 
+	   - Ce module permet de compter le nb de caractères identiques à la même place dans les 2 seéquences.*/
 	char path_input[30];
     char path_input2[30];
 
-	get_path_from_user(path_input); /*on demande à l'utilisateur le fichier de la séquence à étudier*/
+	get_path_from_user(path_input); //On demande à l'utilisateur le fichier de la première séquence à étudier
     printf(CYN "Avec quelle séquence voulez-vous comparer ?\n" RESET);
-	get_path_from_user(path_input2);
+	get_path_from_user(path_input2); //On demande à l'utilisateur le fichier de la deuxième séquence à étudier
 
 	int i;
 	
-	int taille_seq = longueur_sequence(path_input); /*je recupère la longueur de la seq*/
+	int taille_seq = longueur_sequence(path_input); //Permet de récupérer la longueur de la séquence
 	char seq1[taille_seq];
-	extract_sequence(path_input, seq1); /* je stocke mes 2 seq dans 2 tableaux*/
+	extract_sequence(path_input, seq1); //Permet de stocker la première séquence dans un tableau
 	char seq2[taille_seq];
-	extract_sequence(path_input2, seq2);
+	extract_sequence(path_input2, seq2); //Permet de stocker la deuxième séquence dans un tableau
 	
-	int ide = nb_nucleotide_ident(seq1, seq2, taille_seq); /* je compte les diff*/
-	double score = pourct_id(taille_seq, ide); /*conversion en %*/
+	int ide = nb_nucleotide_ident(seq1, seq2, taille_seq); //Recupère le nb de nucléotides identiques entre les deux séquences 
+	double score = pourct_id(taille_seq, ide); //Conversion en % de l'identité de séquence
 	char id[taille_seq];
-	seq_id(taille_seq, seq1, seq2, id); /* création de la seq id pour l'affichage*/
+	seq_id(taille_seq, seq1, seq2, id); //Création de la seq id pour l'affichage
 
+	//Affichage dans le terminal : 
 	printf("\n" underscore bright "Identité de séquence:" RESET " \t%d/%d, soit %.2f %%.\n", ide, taille_seq, score);
 	printf(BLU "seq1\t" RESET);
 
-	for (i=0; i<taille_seq; i++) { printf("%c", seq1[i]); } printf("\n"); /*affichage des seq*/
+	for (i=0; i<taille_seq; i++) { printf("%c", seq1[i]); } printf("\n"); 
 	printf(MAG "seq2\t" RESET);
 
 	for (i=0; i<taille_seq; i++) { printf("%c", seq2[i]); } printf("\n");
