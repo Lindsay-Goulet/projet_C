8b4ba6c442088388d0117c62aced62ce5e4837d1
ambrebaumann <ambrebmn99@gmail.com>
2021-12-15 13:20:45 +0100

    commentaires

--- module_transcription.c
+++ module_transcription.c
@@ -7,6 +7,7 @@
 #define RESET "\x1B[0m"
 
 void transcription(char* sequence, int taille_seq) {
+    //Procédure qui transcrit une séquence, remplace les T par des U.
     int i;
     for (i=0; i<taille_seq; i++) {
         if (sequence[i] == 'T') {
@@ -16,29 +17,31 @@ void transcription(char* sequence, int taille_seq) {
 }
 
 void module_transcription_sequence() {
+    //Procédure qui permet de créer un nouveau fichier avec le brin transcrit
+    
     char path_input[30];
     char path_output[30];
     int v;
 
     do {
 
-        get_path_from_user(path_input);
+        get_path_from_user(path_input); //Demande le fichier à transcrire
 
         int taille_seq = longueur_sequence(path_input);
         char sequence[taille_seq];
-	    extract_sequence(path_input, sequence);
+	    extract_sequence(path_input, sequence); //Permet de stocker la séquence dans un tableau
 
-        v = verification_sequence(path_input, sequence, taille_seq);
+        v = verification_sequence(path_input, sequence, taille_seq); //Permet de vérifier si la séquence est codante
 
-        if (v == 1) {
+        if (v == 1) { //Si la séquence est codante
             printf(CYN "Entrez le nom du fichier de votre séquence transcrite.\n" RESET);
-            get_path_from_user(path_output);
-            transcription(sequence, taille_seq);
-            save_sequence(path_output, sequence);
+            get_path_from_user(path_output); //Demande à l'utilisateur le nom du fichier de sortie
+            transcription(sequence, taille_seq); //Création de la séquence transcrite
+            save_sequence(path_output, sequence); //Permet de sauvegarder la nouvelle séquence transcrite dans un fichier
         }
-        else {
+        else { //Si la séquence est non codante
             printf(RED "Erreur, séquence non codante.\nDonnez un autre fichier.\n" RESET);
         }
     }
-    while(v == 0);
+    while(v == 0); //Faire toutes les instructions passées tant que la séquence n'est pas codante
 }
--- utiles.c
+++ utiles.c
@@ -13,20 +13,25 @@
 
 
 void get_path_from_user(char* path_input) {
-/*Procédure qui permet à l'utilisateur d'entrer le fichier*/
+	//Procédure qui permet à l'utilisateur d'entrer le fichier qu'il veut que le logiciel analyse
+	
 	printf(CYN "Saisissez le nom de votre fichier : " RESET);
 	scanf("%40s", path_input);
 }
 
 void get_module_number_from_user(char* module_number) {
-	    printf(CYN underscore "\nQuel module voulez-vous utiliser ?"RESET"\n1. Recherche de la séquence codante de taille maximale.\n2. Transcription d’une séquence ADN en séquence ARN.\n3. Traduction d’une séquence codante en séquence protéique.\n4. Calcul du score d’identité entre deux séquences.\n5. Calcul du score de similarité de polarité entre deux séquences protéiques.\n6. Recherche d’une séquence consensus à partir d’un alignement multiple.\n7. Recherche de la plus grande sous-chaîne de polarité commune à 2 séquences protéiques.\nq : Quitter.\n"BLU blink bright"Module : "RESET);
-        scanf("\n%c", module_number);
+	//Procédure qui permet à l'utilisateur de choisir le module qu'il veut utiliser ou s'il veut quitter le logiciel
+	//Et qui permet aussi l'affichage dans le terminal des différents choix qui s'offre à l'utilisateur
+	
+	printf(CYN underscore "\nQuel module voulez-vous utiliser ?"RESET"\n1. Recherche de la séquence codante de taille maximale.\n2. Transcription d’une séquence ADN en séquence ARN.\n3. Traduction d’une séquence codante en séquence protéique.\n4. Calcul du score d’identité entre deux séquences.\n5. Calcul du score de similarité de polarité entre deux séquences protéiques.\n6. Recherche d’une séquence consensus à partir d’un alignement multiple.\n7. Recherche de la plus grande sous-chaîne de polarité commune à 2 séquences protéiques.\nq : Quitter.\n"BLU blink bright"Module : "RESET);
+    scanf("\n%c", module_number);
 }
 
 void supp_premiere_ligne(const char* path_input) {
+	//Procédure qui supprime la ligne d'information du fichier FASTA (commence par >) 
 	int caractere_ligne=0;
 	
-	FILE* f = fopen(path_input, "r"); /* ouverture du fichier*/
+	FILE* f = fopen(path_input, "r"); //Ouverture du fichier de l'utilisateur
 		if (!f) {
 			printf(RED "L'ouverture a échoué.\n" RESET);
 			
@@ -38,7 +43,7 @@ void supp_premiere_ligne(const char* path_input) {
 	int taille_ligne = max(caractere_ligne, 80);
 	char ligne[taille_ligne+1];
 		
-	FILE* f2 = fopen("fichier_temporaire.fa", "w"); /* ouverture du fichier*/
+	FILE* f2 = fopen("fichier_temporaire.fa", "w"); //Ouverture du fichier temporaire 
 		if (!f2) {
 			printf(RED "L'ouverture a échoué.\n" RESET);
 			exit(EXIT_FAILURE);
@@ -58,7 +63,7 @@ void supp_premiere_ligne(const char* path_input) {
 }
 
 void extract_sequence(const char* path_input, char* sequence) {
-/*Procédure qui extrait la séquence du fichier pour la mettre dans un tableau séquence*/
+//Procédure qui extrait la séquence du fichier pour la mettre dans un tableau séquence
 
 	supp_premiere_ligne(path_input);
 
@@ -83,7 +88,7 @@ void extract_sequence(const char* path_input, char* sequence) {
 }
 
 void save_sequence(const char* path_output, char* sequence) {
-/*Procédure enregistre une séquence dans un nouveau fichier*/
+//Procédure enregistre une séquence dans un nouveau fichier au format FASTA
     int i=0;
 
     FILE* f = fopen(path_output, "w"); /* ouverture du fichier*/
@@ -91,10 +96,10 @@ void save_sequence(const char* path_output, char* sequence) {
 			printf(RED "L'ouverture a échoué.\n" RESET);
 			exit(EXIT_FAILURE);
 		}
-	while (sequence[i] != '\0') { /*on parcourt toute la séquence stockée dans la variable séquence*/
+	while (sequence[i] != '\0') { //On parcourt toute la séquence stockée dans la variable séquence
         	fputc(sequence[i], f);
         	i++;
-        	if (i%80 == 0) { /*quand on arrive au 80e caractère, on passe à la ligne*/
+        	if (i%80 == 0) { //Quand on arrive au 80e caractère, on passe à la ligne pour respecter le format FASTA
             		fprintf(f, "\n");
         	}
     }
@@ -102,17 +107,17 @@ void save_sequence(const char* path_output, char* sequence) {
 }
 
 int longueur_sequence(const char* path_input) {
-    /*ajouter procédure qui supprime la ligne avant la sequence*/
+    //Fonction qui renvoie la longueur de la séquence
     int taille_seq=0;
 	int c;
 
-    FILE* f = fopen(path_input, "r"); /* ouverture du fichier*/
+    FILE* f = fopen(path_input, "r"); //Ouverture du fichier
 	if (!f) {
 		printf(RED "L'ouverture a échoué.\n" RESET);
 		exit(EXIT_FAILURE);
 	}
 
-	while (fscanf(f, "%lc", &c) != EOF) { /*je parcours tout le fichier et je compte les éléments*/
+	while (fscanf(f, "%lc", &c) != EOF) { //Parcours tout le fichier et compte les éléments
 		if (c != '\n' && c!= '\0') {
 			taille_seq++;
 		}
@@ -123,19 +128,22 @@ int longueur_sequence(const char* path_input) {
 }
 
 int polarite(char aa) {
+	//Fonction qui retourne 0 si l'acide aminé est polaire et 1 sinon.
 	if ((aa == 'C') || (aa == 'Y') || (aa == 'T') || (aa == 'S') || (aa == 'N') || (aa == 'Q') || (aa == 'H') || (aa == 'K') || (aa == 'R') || (aa == 'D') || (aa == 'E')) {
-		return 0; /*retourne pol*/
+		return 0; //Retourne aa polaire
 	}
 	else 
-		return 1; /*retourne apol */
+		return 1; //Retourne aa apolaire
 }
 
 double pourct_id(double taille_seq, int nb_id) {
-	double id = nb_id*100/taille_seq; /*produit en croix pour calculer le pourcentage*/
+	//Fonction qui renvoie le pourcentage d'identité
+	double id = nb_id*100/taille_seq; //Produit en croix pour calculer le pourcentage
 	return id;
 }
 
 int verification_sequence(char* path_input, char* sequence, int taille_seq) {
+	//Fonction qui renvoie 1 si la séquence est codante et 0 sinon.
     extract_sequence(path_input, sequence);
 
     if ((sequence[0] == 'A') && ((sequence[1] == 'T') || (sequence[1] == 'U')) && (sequence[2] == 'G') 
@@ -143,12 +151,11 @@ int verification_sequence(char* path_input, char* sequence, int taille_seq) {
 	|| ((sequence[taille_seq-3] == 'T' || sequence[taille_seq-3] == 'U') && sequence[taille_seq-2] == 'A' && sequence[taille_seq-1] == 'A') 
 	|| ((sequence[taille_seq-3] == 'T' || sequence[taille_seq-3] == 'U') && sequence[taille_seq-2] == 'A' && sequence[taille_seq-1] == 'G'))
 	&& (taille_seq%3 == 0)) {
-        return 1;
-/*ne pas oublier les U*/ /* C'EST FAIT !*/
+		return 1; //Codante
     }
 
     else { 
-        return 0;
+        return 0; //Non codante
     }
 }
 
